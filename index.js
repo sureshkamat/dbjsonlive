883c1d6aeb2c642972fbf9632969ebd820eda671
Suresh Kamat <sureshkamat108@gmail.com>
2023-08-26 12:46:44 +0530

    db json

--- index.js
+++ index.js
@@ -1,35 +1,12 @@
-const jsonServer = require('json-server');
-const cors = require('cors');
-const path = require('path');
-
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const router = jsonServer.router("db.json"); // <== Will be created later
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; // <== You can change the port
 
-server.use(cors({
-    origin:"*"
-}));
-server.use(jsonServer.bodyParser);
 server.use(middlewares);
-
-// Define a custom PATCH route to handle updates
-server.patch('/data/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const updatedData = req.body;
-
-  // Update the data in the "data" collection in db.json
-  router.db.get('data')
-    .find({ id })
-    .assign(updatedData)
-    .write();
-
-  res.json(updatedData);
-});
-
 server.use(router);
 
-const PORT = 8000;
+server.listen(port);
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`);
-});
+module.exports=server
