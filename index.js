d1eabbc236b4d2b88ce9ae3fa2bb8221472ef55b
Suresh Kamat <sureshkamat108@gmail.com>
2023-08-26 12:26:33 +0530

    db json

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
+const jsonServer = require('json-server');
+const cors = require('cors');
+const path = require('path');
 
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
 
-server.use(cors())
-server.use(jsonServer.bodyParser)
-server.use(middlewares)
-server.use(router)
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
 
-const PORT = 8000
+// Define a custom PATCH route to handle updates
+server.patch('/data/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const updatedData = req.body;
+
+  // Update the data in the "data" collection in db.json
+  router.db.get('data')
+    .find({ id })
+    .assign(updatedData)
+    .write();
+
+  res.json(updatedData);
+});
+
+server.use(router);
+
+const PORT = 8000;
 
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
-})
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
